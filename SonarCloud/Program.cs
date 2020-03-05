53b55ae972d443c9f3f378a78d0893a35facd0a8
iamsap <robbymillsap@gmail.com>
2020-03-04 16:08:54 -0800

    Removed Main()

--- SonarCloud/Program.cs
+++ SonarCloud/Program.cs
@@ -12,11 +12,6 @@ namespace SonarCloud
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
